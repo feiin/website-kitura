20dbdfe4f7881816f763061493d9d558e5c0006d
feiin <feiin@qq.com>
2017-05-01 09:14:19 +0800

    fixed

--- Sources/Controllers/ArticleController.swift
+++ Sources/Controllers/ArticleController.swift
@@ -8,7 +8,7 @@ import LoggerAPI
 
 public final class ArticleController {
     
-   public class func List(request: RouterRequest, response: RouterResponse, next: () -> Void) throws {
+    public class func List(request: RouterRequest, response: RouterResponse, next: () -> Void) throws {
         
         var pn = 1
         let pageSize = 10;
@@ -19,16 +19,16 @@ public final class ArticleController {
         {
             let articles = try Article.query().limit(10,withOffset: (pn-1)*pageSize).run().makeDictionary()
             let totalCount = try Article.query().count()
-
+            
             var pages = Int(totalCount / pageSize)
             if pages * pageSize < totalCount {
                 pages += 1
             }
-
+            
             var pager =  ["totalPage":pages,"pageSize":pageSize, "currentPage":pn, "totalCount":totalCount,"nextPage":pn+1,"prevPage":pn-1]
             try response.render("article_list.stencil", context: ["articles":articles,"pager":pager]).end()
-        
-           // try response.status(.OK).send(json: JSON(articles)).end()
+            
+            // try response.status(.OK).send(json: JSON(articles)).end()
             
         }catch QueryError.notSupported(let msg) {
             Log.info(msg)
