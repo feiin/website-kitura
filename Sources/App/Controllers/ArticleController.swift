a6b57f33983fb02da84d8b04658bd02250464b27
feiin <feiin@qq.com>
2017-05-08 23:47:28 +0800

    re-architecture

--- Sources/App/Controllers/ArticleController.swift
+++ Sources/App/Controllers/ArticleController.swift
@@ -16,16 +16,8 @@ public final class ArticleController {
         }
         do
         {
-            let articles = try Article.query().limit(10,withOffset: (pn-1)*pageSize).run().makeDictionary()
-            let totalCount = try Article.query().count()
-            
-            var pages = Int(totalCount / pageSize)
-            if pages * pageSize < totalCount {
-                pages += 1
-            }
-            
-            let pager =  ["totalPage":pages,"pageSize":pageSize, "currentPage":pn, "totalCount":totalCount,"nextPage":pn+1,"prevPage":pn-1]
-            try response.render("article_list.stencil", context: ["articles":articles,"pager":pager]).end()
+            let result = try ArticleRepository.GetArticleList( pn, pageSize: pageSize)
+            try response.render("article_list.stencil", context: result).end()
             
         }catch QueryError.notSupported(let msg) {
             Log.info(msg)
--- Sources/App/Repositories/ArticleRepository.swift
+++ Sources/App/Repositories/ArticleRepository.swift
@@ -1,1 +1,21 @@
 import Foundation
+import Fluent
+import FluentMySQL
+
+class ArticleRepository {
+    
+    class func GetArticleList(_ pageIndex:Int,pageSize:Int) throws -> [String:Any] {
+        
+        let articles = try Article.query().limit(pageSize,withOffset: (pageIndex-1)*pageSize).run().makeDictionary()
+        let totalCount = try Article.query().count()
+        
+        var pages = Int(totalCount / pageSize)
+        if pages * pageSize < totalCount {
+            pages += 1
+        }
+        
+        let pager =  ["totalPage":pages,"pageSize":pageSize, "currentPage":pageIndex, "totalCount":totalCount,"nextPage":pageIndex+1,"prevPage":pageIndex-1]
+        return ["articles":articles,"pager":pager]
+        
+    }
+}
